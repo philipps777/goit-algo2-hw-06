af8eee09f6074a17cd2398efd19ba5a5e33c43ea
philipps777 <7philipps7@gmail.com>
2025-01-27 22:11:17 +0200

    change img

--- task_01.py
+++ task_01.py
@@ -21,21 +21,24 @@ def visualize_top_words(word_frequencies, top_n=10):
     top_words = word_frequencies.most_common(top_n)
     words, frequencies = zip(*top_words)
     plt.bar(words, frequencies)
-    plt.xlabel("Слова")
-    plt.ylabel("Частота")
-    plt.title("Топ слова за частотою використання")
+    plt.xlabel("Words")
+    plt.ylabel("Quantity")
+    plt.title("Top 10 words")
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig("top_words.png")  
-    print("Графік збережено як 'top_words.png'")
+    print("Plot saved as 'top_words.png'")
 
 
 
 if __name__ == "__main__":
-    url = input("Введіть URL для аналізу тексту: ").strip()
+    url = input("Enter URL: ").strip()
     text = fetch_text_from_url(url)
     words = map_func(text)
 
     word_frequencies = reduce_func(words)
 
     visualize_top_words(word_frequencies, top_n=10)
+
+
+    #  url = "https://www.gutenberg.net.au/ebooks01/0100021.txt"
